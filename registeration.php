4e29ef03dd156f36e5930b8dad7334e3d848bc94
James Hamm <jameshamm1995@gmail.com>
2015-05-04 18:40:49 +0100

    buttons for all

---
+++ registeration.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link id="pstyle" type="text/css" rel="stylesheet" href="../CSS/homestyle.css"/>
+		<title>Music Zombie</title>
+		<link rel="icon" href="zombie.ico"/>
+	</head>
+	<body>
+		<?php include ('../jscript-and-php/header.php'); ?>
+		<section id="secondhead">
+			<nav>
+				<ul>
+				 
+				<!-- The searchbar, login button, basket button etc.. will go in here, this has no css yet -->
+				</ul>
+			</nav>
+		</section>
+				<div id="wrapper">
+				<section id="left">
+				
+				<form>
+				User-name: <input type="text" name="username">
+				Email: <input type="text" name="email">
+				Password: <input type="password" name="psw">
+				Retype password: <input type="password" name="psd2">
+				<input type="submit">
+				</form>
+				
+				<section>
+				
+				</div>
+		
+		
+		<?php include ('../jscript-and-php/footer.php'); ?>
+		</body>
+		</html>
